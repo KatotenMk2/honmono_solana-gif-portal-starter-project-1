a4d8acc2ed469eeb519c8d2e5164f19ccfc88133
KatotenMk2 <cino1031@gmail.com>
2022-09-07 21:42:16 +0900

    b

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 
 import idl from './idl.json';
 import kp from './keypair.json'
+require('dotenv').config();
 
 // SystemProgramはSolanaランタイムへの参照です。
 const { SystemProgram, Keypair } = web3;
@@ -23,7 +24,7 @@ const baseAccount = web3.Keypair.fromSecretKey(secret)
 const programID = new PublicKey(idl.metadata.address);
 
 // ネットワークをDevnetに設定します。
-const network = clusterApiUrl('devnet');
+const network = clusterApiUrl(process.env.SOLANA_NETWORK);
 
 // トランザクションが完了したときに通知方法を制御します。
 const opts = {
